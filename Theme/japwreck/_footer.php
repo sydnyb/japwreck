baf30eae9a1138baf544fc7b2765968156e76f86
sybro <nayab99@gmail.com>
2018-11-15 23:46:03 +0530

    Footer

--- Theme/japwreck/_footer.php
+++ Theme/japwreck/_footer.php
@@ -1,17 +1,171 @@
-<footer class="clearfix">
-  <div class="col_12">
-    <?php echo ipSlot('text', array('id' => 'themeName', 'tag' => 'div', 'default' => __('Theme "Air"', 'Air', false), 'class' => 'left')); ?>
-    <div class="right">
-      <?php echo sprintf(__('Drag & drop with %s', 'Air'), '<a href="http://www.impresspages.org">ImpressPages</a>'); ?>
+<!-- Testimonials-->
+<section class="section-80 section-md-top-65 section-md-bottom-100 bg-gray-darkest context-dark section-skew section-skew-wide">
+  <div class="skew-block"></div>
+  <div class="shell text-md-left">
+    <h2>Testimonials</h2>
+    <hr class="divider divider-lg bg-white hr-md-left-0">
+    <div class="offset-top-50">
+      <div data-loop="false" data-items="1" data-dots="true" data-mouse-drag="false" data-sm-items="2" data-nav="false"
+        data-margin="30" class="owl-carousel owl-carousel-default">
+        <div>
+          <div class="quote">
+            <div class="unit unit-md unit-md-horizontal text-md-left">
+              <div class="unit-left"><img src='<?php echo ipThemeUrl("assets/images/users/user-evelyn-fisher-140x140.jpg"); ?>'
+                  width="140" height="140" alt="" class="img-responsive reveal-inline-block img-circle"></div>
+              <div class="unit-body">
+                <div class="inset-md-right-40">
+                  <p>
+                    <q>We have used 'ACR Auto' exclusively for 12 years now. They have serviced 10 different vehicles
+                      for us in that time, and have always been fast, efficient, fair, honest, and trustworthy!</q>
+                  </p>
+                  <p>
+                    <cite>Evelyn Fisher</cite>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="quote">
+            <div class="unit unit-md unit-md-horizontal text-md-left">
+              <div class="unit-left"><img src=<?php echo ipThemeUrl("assets/images/users/user-lawrence-sims-140x140.jpg")
+                  ?>
+                width="140" height="140"
+                alt="" class="img-responsive reveal-inline-block img-circle"></div>
+              <div class="unit-body">
+                <div class="inset-md-right-40">
+                  <p>
+                    <q>The folks at ACR Auto are the best. I have been there with all of my vehicles with issues
+                      ranging from a bad transmission to brake jobs. They are always honest and do a great job.</q>
+                  </p>
+                  <p>
+                    <cite>Lawrence Sims</cite>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="quote">
+            <div class="unit unit-md unit-md-horizontal text-md-left">
+              <div class="unit-left"><img src=<?php echo ipThemeUrl("assets/images/users/user-cristal-smith-270x270.jpg")
+                  ?>
+                width="140" height="140"
+                alt="" class="img-responsive reveal-inline-block img-circle"></div>
+              <div class="unit-body">
+                <div class="inset-md-right-40">
+                  <p>
+                    <q>These guys do a great work. I’m pretty picky when it comes to my car! Mark is very
+                      knowledgeable and friendly, what a great guy! This is a great place for anyone to get quality
+                      work done.</q>
+                  </p>
+                  <p>
+                    <cite>Cristal Smith</cite>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="quote">
+            <div class="unit unit-md unit-md-horizontal text-md-left">
+              <div class="unit-left"><img src=<?php echo ipThemeUrl("assets/images/users/user-cristal-smith-270x270.jpg");
+                  ?>
+                width="140" height="140" alt=""
+                class="img-responsive reveal-inline-block img-circle"></div>
+              <div class="unit-body">
+                <div class="inset-md-right-40">
+                  <p>
+                    <q>We have used 'ACR Auto' exclusively for 12 years now. They have serviced 10 different vehicles
+                      for us in that time, and have always been fast, efficient, fair, honest, and trustworthy!</q>
+                  </p>
+                  <p>
+                    <cite>Patrick Pool</cite>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="quote">
+            <div class="unit unit-md unit-md-horizontal text-md-left">
+              <div class="unit-left"><img src=<?php echo ipThemeUrl("assets/images/users/user-sam-kromstain-270x270.jpg")
+                  ?>
+                width="140" height="140"
+                alt="" class="img-responsive reveal-inline-block img-circle"></div>
+              <div class="unit-body">
+                <div class="inset-md-right-40">
+                  <p>
+                    <q>The folks at ACR Auto are the best. I have been there with all of my vehicles with issues
+                      ranging from a bad transmission to brake jobs. They are always honest and do a great job.</q>
+                  </p>
+                  <p>
+                    <cite>Sam Kromstain</cite>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="quote">
+            <div class="unit unit-md unit-md-horizontal text-md-left">
+              <div class="unit-left"><img src=<?php echo ipThemeUrl("assets/images/users/user-eva-pool-270x270.jpg") ?>
+                width="140" height="140" alt=""
+                class="img-responsive reveal-inline-block img-circle"></div>
+              <div class="unit-body">
+                <div class="inset-md-right-40">
+                  <p>
+                    <q>These guys do a great work. I’m pretty picky when it comes to my car! Mark is very
+                      knowledgeable and friendly, what a great guy! This is a great place for anyone to get quality
+                      work done.</q>
+                  </p>
+                  <p>
+                    <cite>Eva Pool</cite>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</main>
+<footer class="section-60 page-footer bg-gray-darkest section-skew">
+  <div class="skew-block"></div>
+  <div class="shell">
+    <!--Footer brand--><a href="index.html"><img src=<?php echo ipThemeUrl("assets/images/logo-light-136x75.png") ?>
+      width="136" height="75" alt=""
+      class="img-responsive reveal-inline-block"></a>
+    <div class="offset-top-20">
+      <ul class="list-inline list-inline-dark">
+        <li><a href="#" class="icon icon-xxs fa-facebook"></a></li>
+        <li><a href="#" class="icon icon-xxs fa-twitter"></a></li>
+        <li><a href="#" class="icon icon-xxs fa-pinterest"></a></li>
+        <li><a href="#" class="icon icon-xxs fa-vimeo"></a></li>
+        <li><a href="#" class="icon icon-xxs fa-google"></a></li>
+        <li><a href="#" class="icon icon-xxs fa-rss"></a></li>
+      </ul>
+    </div>
+    <div class="offset-top-20">
+      <p class="text-dark">&copy; <span id="copyright-year"></span> All Rights Reserved Terms of Use and <a href="privacy.html"
+          class="text-dark">Privacy Policy</a></p>
     </div>
   </div>
 </footer>
 </div>
 <?php 
-  // if (!$admin) {
-  //   ipAddJs('assets/js/core.min.js');
-  // } 
+if ( ! ipAdminId()) {
+  echo ipAddJs('assets/js/coree.min.js'); 
+} 
 ?>
+echo ipAddJs('assets/js/core.min.js');
 <?php echo ipAddJs('assets/js/script.js'); ?>
 <?php echo ipJs(); ?>
 
